674b20b7550ed6548614553598385a0b1a1f6530
Jessica_Liu <liuqinyin315@gmail.com>
2025-05-06 23:15:27 +0100

    Initialize the Webpage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <ImageGenerator />
+    </div>
+  );
+}
+
+export default App;

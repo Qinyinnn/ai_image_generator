09e1f1e8b849016e123ed5ed03ab9dfb7b02a3fa
Jessica_Liu <liuqinyin315@gmail.com>
2025-05-07 21:33:42 +0100

    Implement Generator Functions
    Also connect with the OpenAI API

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './ImageGenerator.css'
 import default_image from '../Assets/default_image.jpg'
 
 const ImageGenerator = () => {
+    // Declare a state variable `image_url` with initial value "/"
+    // `setImage_url` is the function used to update this state
+    const[image_url, setImage_url] = useState("/");
+    let inputRef = useRef(null);
+
+    // an environment variable to store the react api key
+    const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+    
+    const imageGenerator = async() =>{
+        if (inputRef.current.value==="") {
+            return 0;
+        }
+
+        // Sends an HTTP POST request to OpenAI’s image generation API, waiting for the server response.
+        const response = await fetch(
+            "https://api.openai.com/v1/images/generations",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json", //tells the server (OpenAI's API) we’re sending JSON data.
+                    Authorization: `Bearer ${apiKey}`,
+                    "User-Agent":"Chrome", // optional: identify the client
+                },
+                body: JSON.stringify({
+                    prompt:`${inputRef.current.value}`, // the prompt text the user entered
+                    n:1, // number of images to generate
+                    size:"512x512", // image size
+                }), //The commas are used inside object and array literals to separate multiple key-value pairs or elements.
+            }
+        ); //Semicolons mark the end of a complete JavaScript statement (no need for the closing brace for the whole class)
+
+        let data = await response.json(); //Wait for the openai api’s JSON response and parse it into JS
+        console.log(data); //prints the data to your browser’s console
+
+    }
+
+
     return (
         <div className = 'ai-image-generator'>
             <div className = "header">AI Image <span>Generator</span></div>
             <div className="img-loading">
-                <div className="image"><img src={default_image} alt="" /></div>
+                {/*If `image_url` equals "/", use the default placeholder image.
+                Otherwise, use the actual image URL stored in state. */}
+                <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
 
             </div>
             <div className="search-box">
-                <input type = "text" className = 'search-input' placeholder='Describe What You Want to See'/>
-                <div className="generate-btn">Generate</div>
+            <input ref={inputRef} type = "text" className = 'search-input' placeholder='Describe What You Want to See'/>
+                 {/*When you click on the 'Generate' button, it will run the imageGenerator function */}
+                <div className="generate-btn" onClick={()=>{imageGenerator()}}>Generate</div>
             </div>
         </div>
     )

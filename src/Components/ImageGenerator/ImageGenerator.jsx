f4c4573bdd0284b21200ef600c72cb0a5f645a1e
Jessica_Liu <liuqinyin315@gmail.com>
2025-05-09 23:28:50 +0100

    Implement Image display and loading

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,6 +7,7 @@ const ImageGenerator = () => {
     // `setImage_url` is the function used to update this state
     const[image_url, setImage_url] = useState("/");
     let inputRef = useRef(null);
+    const [loading, setLoading] = useState(false);
 
     // an environment variable to store the react api key
     const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
@@ -16,6 +17,7 @@ const ImageGenerator = () => {
             return 0;
         }
 
+        setLoading(true); //loading starts (on user interface)
         // Sends an HTTP POST request to OpenAI’s image generation API, waiting for the server response.
         const response = await fetch(
             "https://api.openai.com/v1/images/generations",
@@ -24,7 +26,6 @@ const ImageGenerator = () => {
                 headers:{
                     "Content-Type":"application/json", //tells the server (OpenAI's API) we’re sending JSON data.
                     Authorization: `Bearer ${apiKey}`,
-                    "User-Agent":"Chrome", // optional: identify the client
                 },
                 body: JSON.stringify({
                     prompt:`${inputRef.current.value}`, // the prompt text the user entered
@@ -35,8 +36,9 @@ const ImageGenerator = () => {
         ); //Semicolons mark the end of a complete JavaScript statement (no need for the closing brace for the whole class)
 
         let data = await response.json(); //Wait for the openai api’s JSON response and parse it into JS
-        console.log(data); //prints the data to your browser’s console
-
+        let data_array = data.data;
+        setImage_url(data_array[0].url);
+        setLoading(false);
     }
 
 
@@ -46,7 +48,17 @@ const ImageGenerator = () => {
             <div className="img-loading">
                 {/*If `image_url` equals "/", use the default placeholder image.
                 Otherwise, use the actual image URL stored in state. */}
-                <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
+                <div className="image">
+                <img
+                    src={image_url === "/" ? default_image : image_url}
+                    alt=""
+                    width="512"
+                />
+                </div>
+                <div className="loading">
+                    <div className={loading?"loading-bar-full":"loading-bar"}></div>
+                    <div className={loading?"loading-text":"display-none"}>Loading....</div>
+                </div>
 
             </div>
             <div className="search-box">
